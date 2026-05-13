6cfcddcc227395e48168d6349fc8086c56613db0
AlmazBag <almazbagmanov@yandex.ru>
2026-05-13 14:18:23 +0300

    Added gradle-wrapper classpath

--- composeApp/src/commonMain/kotlin/com/bagmanov/kmpnotes/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.bagmanov.kmpnotes
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/bagmanov/kmpnotes/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.bagmanov.kmpnotes
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
